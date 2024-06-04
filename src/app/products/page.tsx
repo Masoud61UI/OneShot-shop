f07cd7ebab634f8377b02a1fb20e22d5864baaf5
Masoud61UI <masoud6181@gail.com>
2024-06-05 00:22:01 +0330

    Fixing some mistakes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -15,7 +15,7 @@ export default async function page() {
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 mt-5">
           {resultProducts.map((item) => {
             return (
-              <Link key={item.id} href={`/productDetails/${item.id}`}>
+              <Link key={item.id} href={`/products/${item.id}`}>
                 <ProductItem {...item} />
               </Link>
             );
